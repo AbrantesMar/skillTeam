72f213c81817249142a9e8e652080e4ca517494f
Marcio Abrantes <abs.marcio@gmail.com>
2018-12-20 01:52:43 -0300

    add Dependency Injection, Repository, TestUnitario, alteração na task para executar o projeto de test

--- skillTeam/SkillTeamTest/UnitTest1.cs
+++ skillTeam/SkillTeamTest/UnitTest1.cs
@@ -1,13 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkillTeam.Controllers;
+using SkillTeam.Models.Repository;
+using Moq;
+using System;
+using MongoDB.Bson;
 
 namespace SkillTeamTest
 {
     [TestClass]
     public class UnitTest1
     {
+        private Mock<IEmployeeRepository> mock;
+
         [TestMethod]
         public void TestMethod1()
         {
+            mock = new Mock<IEmployeeRepository>();
+            mock.Setup(m => m.FindById(ObjectId("507f1f77bcf86cd799439011")).Returns("Testando"));
+            EmployeeController employeeController = new EmployeeController(mock.Object);
+            string result = employeeController.FindEmployeeById(ObjectId("507f1f77bcf86cd799439011")).Description;
+            Console.Write("testando");
+            Assert.AreEqual("Testando", result);
         }
     }
 }
